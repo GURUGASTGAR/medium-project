de2f8e8e5a83b069d345fae5c54206dd8dfd739c
GURUGASTGAR <gurugastgar@gmail.com>
2024-03-18 20:06:48 +0530

    minor changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import { Hono } from 'hono'
-import { userRouter } from '../routes/auth.controller'
-import { blogRouter } from '../routes/blog.controller'
+import { userRouter } from './routes/auth.controller'
+import { blogRouter } from './routes/blog.controller'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
